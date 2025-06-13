1d2a6156eaad9bd2dcb365a179be37ba700d2de4
DNgo-Neumont <dngo@student.neumont.edu>
2025-06-13 02:21:13 -0600

    Updated with fonts and some styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@
 import HeaderNavigationBar from "../components/HeadNavigation"
 
 export default function Page() {
-  return <div>
+  return <div className=" min-h-screen bg-black">
       <HeaderNavigationBar PageList={["test1","test2","test3"]} />
-      <h1 className="flex flex-col justify-center bg-black text-white ">Test</h1>
+      <h1 className="flex flex-col justify-center bg-black text-white font-HelveticaNow">Test</h1>
+      <div className="h-24 text-white font-helvetica_now text-9xl">
+      {/* <div className="h-24 text-white text-9xl font-ferrite_core_dx"> */}
+        Welcome to the future.
+      </div>
     </div>
   
 }
--- src/components/HeadNavigation.tsx
+++ src/components/HeadNavigation.tsx
@@ -6,7 +6,7 @@ export default function HeaderNavigationBar({PageList}:{PageList: Array<String>}
 
 
   const MenuItems = PageList.map((predicate, index) => {
-    return <Link className="flex bg-black text-white" key={index} href={ "/" + predicate }>{predicate}</Link>
+    return <Link className="flex bg-black text-white flex-1/2 justify-center text-4xl pt-5 pb-5 font-helvetica_now" key={index} href={ "/" + predicate }>{predicate}</Link>
   })
 
   return <div className="flex flex-row justify-center">
